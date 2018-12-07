81e34a8df8d03cc279b700a711d68964625503b0
Lauri Kurki <lauri.kurki@siili.com>
2018-12-08 00:03:14 +0200

    better way to deduce dependencies between tasks

--- day_07_sum_of_its_parts/src/main.rs
+++ day_07_sum_of_its_parts/src/main.rs
@@ -3,6 +3,7 @@ extern crate regex;
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::char;
 use regex::Regex;
@@ -20,36 +21,27 @@ fn main() {
     println!("Total time to complete steps in parallel: {}", total_time);
 }
 
-#[derive(Debug)]
-struct Tick {
-    total_time: usize,
-    time_penalty: usize,
-    workers: Vec<(usize, usize)>,
-    nworkers: usize
-}
-
 fn time_to_complete_all_steps(lines: std::str::Lines, workers: usize, time_penalty: usize) -> usize {
-    let network = create_network(lines);
+    let network = create_depnet(lines);
 
-    complete_all_parallel(Tick {
-        total_time: 0,
-        time_penalty: time_penalty,
-        workers: vec![(0, 0); workers],
-        nworkers: workers
-    }, network)
+    complete_all_parallel(vec![(0, ' '); workers], workers, network, time_penalty, 0)
 }
 
-fn complete_all_parallel(mut x: Tick, mut network: (Vec<Vec<usize>>, HashSet<usize>)) -> usize {
-    let available: VecDeque<usize> = find_n_available_instructions(&network.0, &network.1, x.nworkers);
+fn complete_all_parallel(workers: Vec<(usize, char)>,
+                         nworkers: usize,
+                         mut network: HashMap<char, HashSet<char>>,
+                         time_penalty: usize,
+                         total_time: usize) -> usize {
+    let available: VecDeque<char> = find_n_available_instructions(&network, nworkers);
     // Give idle workers a new task if there is any available:
-    let workers = assign_tasks(&x, available_tasks(available, &x));
+    let workers = assign_tasks(&workers, available_tasks(available, &workers), time_penalty);
     let min_completion_time = min_completion_time(&workers);
 
     match min_completion_time {
         Some(min_time) => {
             // Substract the shortest completion time from tasks,
             // if any tasks finished, remove them:
-            let workers: Vec<(usize, usize)> = workers.iter()
+            let workers: Vec<(usize, char)> = workers.iter()
                 .map(|(time, task)| {
                     match time == &0 {
                         true => (*time, *task),
@@ -57,8 +49,8 @@ fn complete_all_parallel(mut x: Tick, mut network: (Vec<Vec<usize>>, HashSet<usi
                             let new_time = time - min_time;
                             match new_time == 0 {
                                 true => {
-                                    network.1.remove(task);
-                                    (0, 0)
+                                    remove_task(&mut network, &task);
+                                    (0, ' ')
                                 },
                                 false => {
                                     (new_time, *task)
@@ -69,32 +61,29 @@ fn complete_all_parallel(mut x: Tick, mut network: (Vec<Vec<usize>>, HashSet<usi
                 })
                 .collect();
 
-            x.total_time += min_time;
-            x.workers = workers;
-
-            complete_all_parallel(x, network)
+            complete_all_parallel(workers, nworkers, network, time_penalty, total_time + min_time)
         },
         None => {
-            x.total_time
+            total_time
         }
     }
 }
 
-fn min_completion_time(workers: &Vec<(usize, usize)>) -> Option<usize> {
+fn min_completion_time<T>(workers: &Vec<(usize, T)>) -> Option<usize> {
     workers.iter()
         .filter(|(time, _)| time != &0)
         .map(|(time, _)| *time)
         .min()
 }
 
-fn assign_tasks(x: &Tick, mut available: VecDeque<usize>) -> Vec<(usize, usize)> {
-    x.workers.iter()
+fn assign_tasks(workers: &Vec<(usize, char)>, mut available: VecDeque<char>, time_penalty: usize) -> Vec<(usize, char)> {
+    workers.iter()
         .map(|(time, task)| {
-            match time == &0 && task == &0 {
+            match time == &0 {
                 true => {
                     let t = available.pop_front();
                     match t {
-                        Some(n) => (1 + n + x.time_penalty, n),
+                        Some(n) => (char_to_seconds(&n) + time_penalty, n),
                         None => (*time, *task)
                     }
                 },
@@ -104,8 +93,8 @@ fn assign_tasks(x: &Tick, mut available: VecDeque<usize>) -> Vec<(usize, usize)>
         .collect()
 }
 
-fn available_tasks(available: VecDeque<usize>, current: &Tick) -> VecDeque<usize> {
-    let current_tasks: Vec<usize> = current.workers.iter()
+fn available_tasks(available: VecDeque<char>, workers: &Vec<(usize, char)>) -> VecDeque<char> {
+    let current_tasks: Vec<char> = workers.iter()
         .filter(|(time, _)| time > &0)
         .map(|(_, task)| *task)
         .collect();
@@ -118,69 +107,77 @@ fn available_tasks(available: VecDeque<usize>, current: &Tick) -> VecDeque<usize
 }
 
 fn instruction_order(lines: std::str::Lines) -> String {
-    let (network, valid) = create_network(lines);
-    let order = find_instruction_order(&network, valid, Vec::new());
+    let network = create_depnet(lines);
+    let order = find_instruction_order(network, Vec::new());
 
     order.iter().collect()
 }
 
-fn create_network(lines: std::str::Lines) -> (Vec<Vec<usize>>, HashSet<usize>) {
-    let (network, valid) = lines
-        .map(|x| {
-            let (dependee, dependent) = parse_line_to_tuple(x);
-            (transform_char(dependee), transform_char(dependent))
-        } )
-        .fold((vec![vec![0; 26]; 26], HashSet::new()),
-              |(mut network, mut valid), (dependee, dependent)| {
-                  valid.insert(dependee);
-                  valid.insert(dependent);
-                  network[dependent][dependee] = 1;
-
-                  (network, valid)
-              });
-
-    (network, valid)
-}
-
-fn find_instruction_order(network: &Vec<Vec<usize>>,
-                         mut valid: HashSet<usize>,
-                         mut result: Vec<char>) -> Vec<char> {
-    let available = find_n_available_instructions(network, &valid, 1);
+fn find_instruction_order(mut network: HashMap<char, HashSet<char>>,
+                          mut result: Vec<char>) -> Vec<char> {
+    let available = find_n_available_instructions(&network, 1);
 
     match available.len() {
          1 => {
-            let c = char::from_u32(available[0] as u32 + 'A' as u32).unwrap();
-            valid.remove(&available[0]);
-            result.push(c);
-            find_instruction_order(network, valid, result)
+             let done = available[0];
+
+             remove_task(&mut network, &done);
+             result.push(done);
+             find_instruction_order(network, result)
         },
         _ => result
     }
 }
 
-fn find_n_available_instructions(network: &Vec<Vec<usize>>, valid: &HashSet<usize>, n: usize) -> VecDeque<usize> {
-    let available: VecDeque<usize> = network.iter()
-        .enumerate()
-        .filter(|(i, _)| valid.contains(i))
-        .map(|(i, x)| -> (usize, usize) {
-            let sum = x.iter()
-                .enumerate()
-                .filter(|(j, _)| valid.contains(j))
-                .map(|(_, y)| y)
-                .sum();
+fn find_n_available_instructions(network: &HashMap<char, HashSet<char>>, n: usize) -> VecDeque<char> {
+    let mut available: Vec<char> = network.iter()
+        .filter(|(_, dependencies)| dependencies.len() == 0)
+        .map(|(task, _)| *task)
+        .collect::<Vec<char>>();
 
-            (i, sum)
-        })
-        .filter(|(_, sum)| sum == &0)
-        .map(|(i, _)| i)
-        .take(n)
-        .collect();
+    available.sort();
 
     available
+        .iter()
+        .map(|x| *x)
+        .take(n)
+        .collect()
 }
 
-fn transform_char(c: char) -> usize {
-    c as usize - 'A' as usize
+fn create_depnet(lines: std::str::Lines) -> HashMap<char, HashSet<char>> {
+    let depnet = lines.
+        fold(HashMap::new(), |mut map: HashMap<char, HashSet<char>>, x| {
+            let (dependee, dependent) = parse_line_to_tuple(x);
+
+            map.entry(dependee)
+                .or_insert(HashSet::new());
+
+            map.entry(dependent)
+                .and_modify(|v| {
+                    v.insert(dependee);
+                })
+                .or_insert({
+                    let mut set = HashSet::new();
+                    set.insert(dependee);
+                    set
+                });
+
+            map
+        });
+
+    depnet
+}
+
+fn remove_task(network: &mut HashMap<char, HashSet<char>>, task: &char) {
+    for dependent in network.values_mut() {
+        dependent.remove(task);
+    }
+
+    network.remove(task);
+}
+
+fn char_to_seconds(c: &char) -> usize {
+    1 + ((*c as usize) - 'A' as usize)
 }
 
 fn parse_line_to_tuple(line: &str) -> (char, char) {
