39e56efe4a5c0f269f9441bbf84eb0a2d1c7c174
Lauri Kurki <lauri.kurki@siili.com>
2018-12-08 13:48:13 +0200

    day 8 part 2 ready

--- day_08_memory_maneuver/src/main.rs
+++ day_08_memory_maneuver/src/main.rs
@@ -15,6 +15,43 @@ fn main() {
     let sum: usize = metadata.iter().sum();
 
     println!("Sum of metadata values: {}", sum);
+
+    let tree = parse_string_to_vec(&buffer);
+    let (root_value, _) = find_root_node_value(tree);
+    println!("The value of root node: {}", root_value);
+}
+
+fn find_root_node_value(mut tree: VecDeque<usize>) -> (usize, VecDeque<usize>) {
+    let nchild = tree.pop_front().unwrap();
+    let nmeta = tree.pop_front().unwrap();
+
+    match nchild {
+        0 => {
+            let (values, tree) = take_n_metavalues(tree, nmeta);
+            (values.iter().sum(), tree)
+        },
+        _ => {
+            let (child_node_values, new_tree) = (0..nchild)
+                .fold((Vec::new(), tree), |(mut metasums, new_tree), _| {
+                    let (value, t) = find_root_node_value(new_tree);
+                    metasums.push(value);
+
+                    (metasums, t)
+                });
+
+            let (mut values, new_tree) = take_n_metavalues(new_tree, nmeta);
+
+            let value = values.iter()
+                .filter(|x| x > &&0)
+                .map(|x| match child_node_values.get(x - 1) {
+                    Some(n) => n,
+                    None => &0
+                })
+                .sum();
+
+            (value, new_tree)
+        }
+    }
 }
 
 fn gather_metadata(mut tree: VecDeque<usize>) -> (Vec<usize>, VecDeque<usize>) {
@@ -40,7 +77,6 @@ fn gather_metadata(mut tree: VecDeque<usize>) -> (Vec<usize>, VecDeque<usize>) {
             (metadata, new_tree)
         }
     }
-
 }
 
 fn take_n_metavalues(mut tree: VecDeque<usize>, n: usize) -> (Vec<usize>, VecDeque<usize>) {
@@ -71,5 +107,13 @@ mod tests {
         let sum: usize = metadata.iter().sum();
 
         assert_eq!(sum, 138);
+    }
+
+    #[test]
+    fn test_find_root_node_value() {
+        let tree = parse_string_to_vec("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2");
+        let (root_value, _) = find_root_node_value(tree);
+
+        assert_eq!(root_value, 66);
     }
 }
